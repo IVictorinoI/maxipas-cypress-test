df675bbfe2b690b2833c5b77bcaea1305edd5ba5
Victor Matheus Mendes <victormatheusmendes@hotmail.com>
2022-09-04 19:28:47 -0300

    teste crud grupo

--- cypress/e2e/maxipas/grupo.cy.js
+++ cypress/e2e/maxipas/grupo.cy.js
@@ -8,23 +8,37 @@ context('Login', () => {
     });
 
     it('Listar e cadastrar grupos', () => {
+        //const descricao = 'Teste CYPRESS '+new Date().toLocaleString();
         const descricao = 'Teste CYPRESS';
 
         //cadastrar(descricao);
-        cy.wait(5000)
+
         pesquisar(descricao);
+        
+        excluir(descricao);
     });
 });
-
-function pesquisar(texto) {
-    cy.get('.p-chips-input-token > input').type(texto+'{enter}');
-    cy.get('.p-selectable-row > :nth-child(2)').should('have.text', 'Descrição'+texto)
-}
 
 function cadastrar(descricao) {
     cy.get('.ml-auto > .p-button').click();
 
-    cy.get('#descricao').type('Teste CYPRESS').should('have.value', 'Teste CYPRESS');
+    cy.get('#descricao').type(descricao).should('have.value', descricao);
 
     cy.get('.p-button-primary > .p-button-label').click();
+}
+
+function pesquisar(texto) {
+    aguardarListagemCarregar();
+    cy.get('.p-chips-input-token > input').type(texto+'{enter}');
+    cy.get('.p-selectable-row > :nth-child(2)').should('contain', texto)
+}
+
+function excluir(texto) {
+    cy.get('.p-selectable-row > :nth-child(2)').should('contain', texto)
+    cy.wait(2000)
+    cy.get('.text-right > .p-button-text').type('{enter}');
+}
+
+function aguardarListagemCarregar() {
+    cy.get('.p-selectable-row > :nth-child(1)', { timeout: 10000 }).should('be.visible');
 }
